4e349280c930b52d265181a4750e455fc0aa5e42
Bart van Ommen <bartvanommen@gmail.com>
2022-12-01 23:39:22 +0100

    feat(Day1): Completed Day 1 of Advent of Code 2022

---
+++ Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var topElves = input.Split("\r\n\r\n")
+    .Select(elf => elf.Split("\r\n").Select(int.Parse).Sum())
+    .Order()
+    .Reverse()
+    .ToList();
+
+// Part 1
+Console.WriteLine("Elf with most categories: {0:D}",
+    topElves.Max());
+
+// Part 2
+Console.WriteLine("Sum of top three elves: {0:D}", 
+    topElves.Take(3).Sum());
